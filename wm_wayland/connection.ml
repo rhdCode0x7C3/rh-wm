7b0dc1ab64757589206aab89e8fa56ea2fade688
rhdCode <raisers.asunder.9m@icloud.com>
2026-01-05 18:10:37 +0000

    trying to ennumerate Wayland globals

--- wm_wayland/connection.ml
+++ wm_wayland/connection.ml
@@ -6,5 +6,7 @@ let connection ~net =
   let transport = Unix_transport.connect ~sw ~net () in
   let client = Client.connect ~sw transport in
   let reg = Registry.of_display client in
-  let river = Registry.get_exn reg "river_window_manager_v1" in
-  river
+  let river = Registry.get reg "river_window_manager_v1" in
+  Format.printf "Found %d river entries" (List.length river);
+  let compositor = Registry.get reg "wl_compositor" in
+  Format.printf "Found %d river entries" (List.length compositor)
